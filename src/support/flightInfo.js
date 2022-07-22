da9dc47d99439ee29875e0a5a99ec90f32e1130b
Ricardo Polanski <ricardo.polanski@ericsson.com>
2022-07-22 11:49:29 -0300

    add support folder and moving flight info into

---
+++ src/support/flightInfo.js
@@ -0,0 +1,12 @@
+class FlightData {    
+        from = 'New York';
+        destination = 'Berlin';
+        departureDate = 'Sat, July 30';
+        fromFullValue = "John F. Kennedy International Airport";
+        destinationFullValue = "Berlin";
+        iataFrom = "JFK";
+        iataDestination = "BER";
+        passengerNumbers = '2 passengers';
+}
+
+module.exports = {FlightData}
